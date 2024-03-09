2a239b013451caf91dbdd092a5c41f7f82d6dbfe
FScoward <endlick@gmail.com>
2024-03-10 00:16:05 +0900

    remove unused

--- cli/src/nativeMain/kotlin/Main.kt
+++ cli/src/nativeMain/kotlin/Main.kt
@@ -2,21 +2,8 @@ package com.github.fscoward.txtman.cli
 
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.core.subcommands
-import com.github.ajalt.clikt.parameters.options.*
-import com.github.ajalt.clikt.parameters.types.int
 import com.github.ajalt.clikt.parameters.arguments.argument
 import com.github.fscoward.txtman.cli.model.*
-
-class Hello : CliktCommand() {
-    val count: Int by option().int().default(1).help("Number of greetings")
-    val name: String by option().prompt("Your name").help("The person to greet")
-
-    override fun run() {
-        repeat(count) {
-            echo("Hello $name!")
-        }
-    }
-}
 
 class AddTask: CliktCommand(name = "add", help = "Add a new task") {
     val name by argument()
