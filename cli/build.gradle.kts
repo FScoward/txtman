c867bddc1f3798b1fb429f5004905dbf932358f3
FScoward <endlick@gmail.com>
2024-03-09 19:37:38 +0900

    package

--- cli/build.gradle.kts
+++ cli/build.gradle.kts
@@ -22,7 +22,7 @@ kotlin {
     nativeTarget.apply {
         binaries {
             executable {
-                entryPoint = "main"
+                entryPoint = "com.github.fscoward.txtman.cli.main"
             }
         }
     }
--- cli/src/nativeMain/kotlin/Main.kt
+++ cli/src/nativeMain/kotlin/Main.kt
@@ -1,3 +1,5 @@
+package com.github.fscoward.txtman.cli
+
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.options.help
 import com.github.ajalt.clikt.parameters.options.default
