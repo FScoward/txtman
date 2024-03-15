f1e355bd6825249dc4d17ce592217de658a86be4
FScoward <endlick@gmail.com>
2024-03-15 23:29:23 +0900

    update status

--- cli/src/nativeMain/kotlin/cmd/UpdateTask.kt
+++ cli/src/nativeMain/kotlin/cmd/UpdateTask.kt
@@ -2,14 +2,20 @@ package com.github.fscoward.txtman.cli.cmd
 
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.options.default
 import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.options.switch
 import com.github.fscoward.txtman.cli.model.*
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 
 class UpdateTask : CliktCommand(name = "update") {
     val id by argument()
-    val status by option()
+    val status by option().switch(
+        "--todo" to TaskStatus.TODO,
+        "--wip" to TaskStatus.IN_PROGRESS,
+        "--done" to TaskStatus.DONE
+    ).default(TaskStatus.IN_PROGRESS)
 
     private fun updateTaskStatus(tasks: List<Task>, id: String, newStatus: TaskStatus): List<Task> {
         return tasks.map { task ->
@@ -28,7 +34,7 @@ class UpdateTask : CliktCommand(name = "update") {
 
     override fun run() {
         val taskList = TaskList.load()
-        val updatedTasks = updateTaskStatus(taskList.tasks, id, TaskStatus.IN_PROGRESS)
+        val updatedTasks = updateTaskStatus(taskList.tasks, id, status)
 
         // 変更のログを記録する
         ActivityLog(
