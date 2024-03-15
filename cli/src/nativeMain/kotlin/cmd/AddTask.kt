10350441a6d52e67ea473849bc9af8e816a8eb03
FScoward <endlick@gmail.com>
2024-03-15 22:56:55 +0900

    save to jsonfile

--- cli/src/nativeMain/kotlin/cmd/AddTask.kt
+++ cli/src/nativeMain/kotlin/cmd/AddTask.kt
@@ -2,17 +2,24 @@ package com.github.fscoward.txtman.cli.cmd
 
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.arguments.argument
-import com.github.fscoward.txtman.cli.model.ActionType
-import com.github.fscoward.txtman.cli.model.ActivityLog
-import com.github.fscoward.txtman.cli.model.Task
-import com.github.fscoward.txtman.cli.model.write
+import com.github.fscoward.txtman.cli.model.*
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 
 class AddTask : CliktCommand(name = "add", help = "Add a new task") {
     val name by argument()
     override fun run() {
+        val taskList = TaskList.load()
+
         val newTask = Task(name)
+        val updatedTasks = taskList.tasks + newTask
+        val updatedJson = Json.encodeToString(TaskList(updatedTasks))
+
         ActivityLog(actionType = ActionType.ADD, log = newTask.toJson()).write()
+        TaskList(updatedTasks).save()
+
         echo("add task ${newTask.id}, $newTask")
         echo("add task ${newTask.toJson()}")
+        echo("updatedJson $updatedJson")
     }
 }
--- cli/src/nativeMain/kotlin/model/TaskList.kt
+++ cli/src/nativeMain/kotlin/model/TaskList.kt
@@ -1,10 +1,13 @@
 package com.github.fscoward.txtman.cli.model
 
+import kotlinx.io.Buffer
 import kotlinx.io.buffered
 import kotlinx.io.files.Path
 import kotlinx.io.files.SystemFileSystem
 import kotlinx.io.readString
+import kotlinx.io.writeString
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 
 @Serializable
@@ -22,11 +25,20 @@ data class TaskList(val tasks: List<Task>) {
     fun convertTasksToBulletList(tasks: List<Task>, indentLevel: Int = 0): String {
         val indent = "  ".repeat(indentLevel)
         return tasks.joinToString(separator = "\n") { task ->
-            "$indent- ${task.id}, priority: ${task.priority}, status: ${task.status}, createdDateTime: ${task.createdDateTime}" +
+            "$indent- ${task.id}, name: ${task.name}, priority: ${task.priority}, status: ${task.status}, createdDateTime: ${task.createdDateTime}" +
                     if (task.children.isNotEmpty()) "\n${convertTasksToBulletList(task.children, indentLevel + 1)}" else ""
         }
     }
     override fun toString(): String {
         return convertTasksToBulletList(tasks)
     }
+}
+fun TaskList.save() {
+    // https://fzhinkin.github.io/kotlinx-io-dokka-docs-preview/kotlinx-io-core/index.html
+    val sink = SystemFileSystem.sink(path = Path("./sample.json"), append = false)
+    val buffer = Buffer()
+    val json = Json.encodeToString(this)
+    buffer.writeString(json)
+    buffer.transferTo(sink)
+    sink.flush()
 }
