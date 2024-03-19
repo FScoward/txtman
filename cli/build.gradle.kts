f7d6726cd2416fd8657efc9aad1c44b3161ad716
FScoward <endlick@gmail.com>
2024-03-19 12:07:04 +0900

    settings for unit test

--- cli/build.gradle.kts
+++ cli/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.plugin.mpp.NativeBuildType
+
 plugins {
     kotlin("multiplatform") version "1.9.22"
     kotlin("plugin.serialization") version "1.9.23"
@@ -14,7 +16,7 @@ kotlin {
     val hostOs = System.getProperty("os.name")
     val isMingwX64 = hostOs.startsWith("Windows")
     val nativeTarget = when {
-        hostOs == "Mac OS X" -> macosX64("native")
+        hostOs == "Mac OS X" -> macosArm64("native")
         hostOs == "Linux" -> linuxX64("native")
         isMingwX64 -> mingwX64("native")
         else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
@@ -28,8 +30,16 @@ kotlin {
         }
     }
     sourceSets {
+        val commonMain by getting
+        val commonTest by getting
         val nativeMain by getting
-        val nativeTest by getting
+        val nativeTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+                implementation(kotlin("test-common"))
+                implementation(kotlin("test-annotations-common"))
+            }
+        }
         nativeMain.dependencies {
             implementation("com.github.ajalt.clikt:clikt:4.2.2")
             implementation("com.aallam.ulid:ulid-kotlin:1.3.0")
